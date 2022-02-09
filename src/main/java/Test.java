e81d73c877557d78ae234e7104b8688671979885
alien <alien.pan@nextfortune.tw>
2022-02-09 22:22:58 +0800

    feat: first commit.

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        TwoSum t = new TwoSum();
+
+        int[] tt = new int[] {
+          2, 7, 11, 15
+        };
+
+        tt = t.twoSum(tt, 9);
+    }
+}
+
---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,31 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        int rootIndex = 0;
+        int mvIndex = 1;
+
+        int limit = nums.length - 1;
+
+        while(rootIndex < limit) {
+
+            if(mvIndex > limit) {
+                rootIndex++;
+                mvIndex = rootIndex + 1;
+            }
+
+            int testValue = nums[rootIndex] + nums[mvIndex];
+
+            if(target == testValue) {
+                result[0] = rootIndex;
+                result[1] = mvIndex;
+                break;
+            }
+
+            mvIndex++;
+        }
+
+        return result;
+
+    }
+}
