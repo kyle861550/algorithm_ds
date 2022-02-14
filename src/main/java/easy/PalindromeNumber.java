665b6da493290d7e44c89e67371444506dab84d7
alien <alien.pan@nextfortune.tw>
2022-02-14 22:48:47 +0800

    feat: Add PalindromeNumber.

---
+++ src/main/java/easy/PalindromeNumber.java
@@ -0,0 +1,47 @@
+package easy;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        PalindromeNumber p = new PalindromeNumber();
+
+        System.out.println("121: " + p.isPalindrome(121));
+
+        System.out.println("-121: " + p.isPalindrome(-121));
+
+        System.out.println("10: " + p.isPalindrome(10));
+
+        System.out.println("0: " + p.isPalindrome(0));
+    }
+
+    public boolean isPalindrome(int x) {
+        boolean result = true;
+
+        String s = Integer.toString(x);
+        char[] chars = s.toCharArray();
+
+        int p = 0, e = chars.length - 1;
+        boolean isEven = chars.length % 2 == 0;
+        while (true) {
+            if(isEven) {
+                if(p - e == 1 ) {
+                    break;
+                }
+            } else {
+                if(p == e) {
+                    break;
+                }
+            }
+
+            char startChar = chars[p++];
+            char endChar = chars[e--];
+
+            if(startChar != endChar) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
