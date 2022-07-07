26b613c788fcce295663f133d8b6a3f808894bd8
alien <alien@gmail.com>
2022-07-07 22:01:57 +0800

    feat: make doing dynamic plan

---
+++ src/main/java/learn/dynamic/CancelChildRepeat.java
@@ -0,0 +1,69 @@
+package learn.dynamic;
+
+import java.util.HashMap;
+
+public class CancelChildRepeat {
+
+    int step = 0;
+    private int fibonacciArray(int value) {
+        if(value == 0) return 0;
+
+        int[] memo = new int[value + 1];
+
+        return cancelByArray(value, memo);
+    }
+
+    private int cancelByArray(int value, int[] memo) {
+        if(value == 1 || value == 2) {
+            return 1;
+        }
+
+        if(memo[value] != 0) {
+            System.out.println("value: " + value + ", memo[value]: " + memo[value]);
+            return memo[value];
+        }
+
+        memo[value] = cancelByArray(value - 1, memo) + cancelByArray(value - 2, memo);
+
+        System.out.println("Step: " + ++step);
+
+        return memo[value];
+    }
+
+    private int fibonacciHashMap(int value) {
+        if(value == 0) return 0;
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        return cancelByHashMap(value, hashMap);
+    }
+
+    private int cancelByHashMap(int value, HashMap<Integer, Integer> hashMap) {
+        if(value == 1 || value == 2) {
+            return 1;
+        }
+
+        Integer memoValue = hashMap.get(value);
+        if(memoValue != null && memoValue != 0) {
+            System.out.println("value: " + value + ", memo[value]: " + memoValue);
+            return memoValue;
+        }
+
+        int result = cancelByHashMap(value - 1, hashMap) + cancelByHashMap(value - 2, hashMap);
+
+        hashMap.put(value, result);
+
+        System.out.println("Step: " + ++step);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        CancelChildRepeat repeat = new CancelChildRepeat();
+
+//        System.out.println("fibonacciArray: " + repeat.fibonacciArray(10));
+
+        System.out.println("fibonacciHashMap: " + repeat.fibonacciHashMap(10));
+    }
+
+}
--- src/main/java/learn/dynamic/Fibonacci.java
+++ src/main/java/learn/dynamic/Fibonacci.java
@@ -2,9 +2,19 @@ package learn.dynamic;
 
 public class Fibonacci {
 
-    int fib(int N) {
+    int step = 0;
+    private int fib(int N) {
         if(N == 0) return 0;
         if(N == 1 || N == 2) return 1;
+
+        System.out.println("Step: " + ++step);
+
         return fib(N - 1) + fib(N - 2);
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+
+        System.out.println(fibonacci.fib(10));
     }
 }
